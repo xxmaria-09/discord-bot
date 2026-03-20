15ad84363e75e7c7da5931e0a444a6b42f5c5453
maryh <111790933+xxmaria-09@users.noreply.github.com>
2026-03-20 14:52:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,18 @@ client.on('messageReactionRemove', async (reaction, user) => {
 // ===============================
 client.login(process.env.TOKEN);
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Server running on port ${PORT}`);
+});
+
 
 
 
